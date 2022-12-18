3baab0fbab723c2430d58ad78a550fbea265bb45
PBhandare <param.bhandare@gmail.com>
2022-12-18 17:39:47 -0600

    trying uploads

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {useForm} from 'react-hook-form';
 
 function App() {
+  const {register,  handleSumbit} = useForm()
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit = {handleSubmit(onSubmit)}>
+        <input ref = "register" type = "file"></input>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
